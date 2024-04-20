0b99423caa94abd6ac9311d96e0232a064dab8f6
elyssa-a-adams <elyssaa1995@gmail.com>
2024-04-19 21:46:55 -0400

    mongo?

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import express from "express"
 const app = express();
 app.use(cors());
 app.use(express.json());
-mongoose.connect("mongodb://localhost:27017/Snapper");
+//mongoose.connect("mongodb://localhost:27017/Snapper");
+mongoose.connect(DB_CONNECTION_STRING);
 PostRoutes(app);
 app.listen(process.env.PORT || 4000);
