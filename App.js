8945f5e5f9b4571c49cc95ec8b264c3b18588190
elyssa-a-adams <elyssaa1995@gmail.com>
2024-04-19 23:48:07 -0400

    users

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
+import express from "express";
 import cors from "cors";
-import mongoose from 'mongoose';
-import PostRoutes from './Posts/routes.js';
-import express from "express"
+import UserRoutes from "./Users/routes.js";
+import mongoose from "mongoose";
+import "dotenv/config";
+import session from "express-session";
+import PostRoutes from "./Posts/routes.js";
 const app = express();
-app.use(cors());
 app.use(express.json());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.HTTP_SERVER_DOMAIN,
+  };
+}
+app.use(session(sessionOptions));
 mongoose.connect("mongodb+srv://snapper:bonaire@cluster0.zxeedro.mongodb.net/Snapper?retryWrites=true&w=majority&appName=Cluster0");
 PostRoutes(app);
-app.listen(4000);
+UserRoutes(app);
+app.listen(process.env.PORT || 4000);
--- Posts/schema.js
+++ Posts/schema.js
@@ -2,7 +2,7 @@ import e from "express";
 import mongoose from "mongoose";
 const postSchema = new mongoose.Schema({
     id: { type: String, required: true},
-    userId: { type: String, required: true},
+    username: { type: String, required: true},
     image: String,
     caption: String,
     location: String,
