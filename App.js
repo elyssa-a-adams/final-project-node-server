575d4804674356a9ebb64f99e21716898c16cce0
elyssa-a-adams <elyssaa1995@gmail.com>
2024-04-19 21:19:49 -0400

    post working

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
-import express from "express";
-import session from "express-session";
-import mongoose from "mongoose";
-import PostRoutes from "./Posts/routes.js";
-mongoose.connect("mongodb://localhost:27017/snapper");
+import cors from "cors";
+import mongoose from 'mongoose';
+import PostRoutes from './Posts/routes.js';
+import express from "express"
 const app = express();
+app.use(cors());
 app.use(express.json());
+mongoose.connect("mongodb://localhost:27017/Snapper");
 PostRoutes(app);
 app.listen(process.env.PORT || 4000);
